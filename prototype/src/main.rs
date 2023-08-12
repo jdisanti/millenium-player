c589fcf1f66f7ab1f337f79548bc6e6333ce0abb
John DiSanti <john@vinylsquid.com>
2023-08-12 11:43:53 -0700

    Prototype playing a song and skipping part way into it

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -12,6 +12,37 @@
 // You should have received a copy of the GNU General Public License along with Millenium Player.
 // If not, see <https://www.gnu.org/licenses/>.
 
+use std::{
+    fs::File,
+    io::{BufReader, Cursor, Read},
+    time::Duration,
+};
+
+use rodio::Source;
+
 fn main() {
-    println!("Hello, world!");
+    println!("loading hydrate.mp3 into memory");
+    let mut file = BufReader::new(File::open("../test-data/hydrate/hydrate.mp3").unwrap());
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents).unwrap();
+
+    println!("opening audio output device");
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&stream_handle).unwrap();
+
+    println!("playing first 5 seconds of hydrate.mp3");
+    sink.append(
+        rodio::Decoder::new(Cursor::new(contents.clone()))
+            .unwrap()
+            .take_duration(Duration::from_secs(5)),
+    );
+    sink.sleep_until_end();
+
+    println!("skipping 60 seconds into hydrate.mp3");
+    sink.append(
+        rodio::Decoder::new(Cursor::new(contents.clone()))
+            .unwrap()
+            .skip_duration(Duration::from_secs(60)),
+    );
+    sink.sleep_until_end();
 }
