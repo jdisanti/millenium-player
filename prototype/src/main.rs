cd504ebc9db3ab7fcbaff4543b57a9bddaa9fab1
John DiSanti <john@vinylsquid.com>
2023-08-12 12:12:38 -0700

    Add file drag and drop prototype

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -12,15 +12,30 @@
 // You should have received a copy of the GNU General Public License along with Millenium Player.
 // If not, see <https://www.gnu.org/licenses/>.
 
+use clap::Parser;
+use rodio::Source;
 use std::{
     fs::File,
     io::{BufReader, Cursor, Read},
     time::Duration,
 };
 
-use rodio::Source;
+#[derive(Debug, clap::Subcommand)]
+enum Prototype {
+    /// Runs the prototype for playing audio
+    PlayAudio,
+    /// Eframe drag and drop
+    EframeDragAndDrop,
+}
 
-fn main() {
+#[derive(Debug, clap::Parser)]
+#[command()]
+struct Args {
+    #[command(subcommand)]
+    prototype: Prototype,
+}
+
+fn prototype_play_audio() {
     println!("loading hydrate.mp3 into memory");
     let mut file = BufReader::new(File::open("../test-data/hydrate/hydrate.mp3").unwrap());
     let mut contents = Vec::new();
@@ -45,4 +60,49 @@ fn main() {
             .skip_duration(Duration::from_secs(60)),
     );
     sink.sleep_until_end();
+}
+
+fn prototype_eframe_drag_and_drop() {
+    struct App;
+    impl App {
+        fn new(_creation_context: &eframe::CreationContext<'_>) -> Self {
+            Self
+        }
+    }
+    impl eframe::App for App {
+        fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                ui.vertical_centered(|ui| {
+                    ui.add(egui::Label::new("Drag and drop a file here"));
+                });
+
+                ctx.input(|input| {
+                    if !input.raw.dropped_files.is_empty() {
+                        println!("dropped files: {:?}", input.raw.dropped_files);
+                    }
+                })
+            });
+        }
+    }
+    let native_options = eframe::NativeOptions {
+        app_id: Some("millenium-player-prototype".into()),
+        drag_and_drop_support: true,
+        resizable: true,
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Millenium Player Prototype",
+        native_options,
+        Box::new(|cc| Box::new(App::new(cc))),
+    )
+    .unwrap();
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.prototype {
+        Prototype::PlayAudio => prototype_play_audio(),
+        Prototype::EframeDragAndDrop => prototype_eframe_drag_and_drop(),
+    }
 }
